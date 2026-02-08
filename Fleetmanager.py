2ca5033ade51dcc4eb1ae2f05e4ae0fc6117dbb6
Rios-1234 <rh10@kent.ac.uk>
2026-02-08 13:15:35 +0000

    Update Fleetmanager.py

--- Fleetmanager.py
+++ Fleetmanager.py
@@ -2,6 +2,8 @@
   names = ["Picard", "Riker", "Data", "Worf", "Crusher"]
   ranks = ["Captain", "Cadet", "Lieutenant Commander", "Lieutenant", "Commander"]
   divs =  ["Command", "Command", "Operations", "Operations", "Sciences"]
+  ids = [1001, 1002, 1003, 1004, 1005]
+  return names,ranks,divs,ids
            
    
 

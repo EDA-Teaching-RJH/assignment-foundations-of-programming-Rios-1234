5a46c2fb138d9f2e228ed0431bd17f3871f754a1
Rios-1234 <rh10@kent.ac.uk>
2026-02-11 16:19:09 +0000

    Update Fleetmanager.py fixing typose and indents

--- Fleetmanager.py
+++ Fleetmanager.py
@@ -1,9 +1,9 @@
 def init_database():
-  names = ["Picard", "Riker", "Data", "Worf", "Crusher"]
-  ranks = ["Captain", "Cadet", "Lieutenant Commander", "Lieutenant", "Commander"]
-  divs =  ["Command", "Command", "Operations", "Operations", "Sciences"]
-  ids = [1001, 1002, 1003, 1004, 1005]
-  return names,ranks,divs,ids
+    names = ["Picard", "Riker", "Data", "Worf", "Crusher"]
+    ranks = ["Captain", "Cadet", "Lieutenant Commander", "Lieutenant", "Commander"]
+    divs = ["Command", "Command", "Operations", "Operations", "Sciences"]
+    ids = [1001, 1002, 1003, 1004, 1005]
+    return names, ranks, divs, ids
 
 def display_menu(user):
   print(f"\n Fleet Manager (User: {user})")
@@ -29,7 +29,7 @@ def add_a_crew_member(name,rank,div,id):
   rank.append(rank)
   div.append(div)
   id.appends(new_id)
-  print("sucessfully added")
+  print("added")
  else:
   print("Id already exsists")
 
@@ -68,6 +68,7 @@ def search_crew(name,rank,div,id):
      if search in name [i].lower():
           print(f"{id[i]} | {name[i]} | {rank[i]} | {div[i]}")
    print()
+  
 def filter_by_diviision(name,div):
   div=input("science,commanda,operation")
   print()
@@ -115,6 +116,7 @@ def main():
       elif int(choice) ==9:
         print("excape") 
         break
+main()
       
       
 

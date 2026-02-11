2f2b8604e1adaf413ea34148823e2511ea64528d
Rios-1234 <rh10@kent.ac.uk>
2026-02-11 15:57:17 +0000

    Update Fleetmanager.py

--- Fleetmanager.py
+++ Fleetmanager.py
@@ -22,7 +22,7 @@ def add_a_crew_member(name,rank,div,id):
  name = input("name: ")
  rank = input("rank: ") 
  div = input("division: ")
- new-id = int(input("ID: "))
+ new_id = int(input("ID: "))
 
  if new_ID not in ids:
   names.append(name)
@@ -33,17 +33,17 @@ def add_a_crew_member(name,rank,div,id):
  else:
   print("Id already exsists")
 
-def remove_memeber(name,rank,div,id):
-  remove id=int(input("reomve id: ")
-  if remove_id in id:
-    index = id.index(remove_id)
-    name.pop.(index)
-    rank.pop(index)
-    div.pop(index)
-    id.pop(index)
-    print("memebr removed")
-else:
-    print("Id not ffound ")
+def remove_member(names, ranks, divs, ids):
+    remove_id = int(input("ID to remove: "))
+    if remove_id in ids:
+        index = ids.index(remove_id)
+        names.pop(index)
+        ranks.pop(index)
+        divs.pop(index)
+        ids.pop(index)
+        print("Removed")
+    else:
+        print("ID not found!")
   
 
 def update_ranks(name,rank,id):
@@ -76,18 +76,47 @@ def filter_by_diviision(name,div):
       print(name)
 print()
 
-def calculating_payroll
+def calculating_payroll(ranks):
     pay={
       "captin":1000,
       "cadet":500,
       "lieutenant comander":800,
-      "lieutenant":750
+      "lieutenant":750,
       "comander":900
     }
     return sum(pay.get(rank,0) for rank in ranks)
 
-def counting_officers(ranks)
- return sum(rank in {"captin", "commander"} for rank in ranks
+def counting_officers(ranks):
+ return sum(rank in {"captain", "commander"} for rank in ranks
+
+def main():
+    user = input ("enter your name")
+    name,rank,div,id = init_database()
+
+    while true:
+      choice = display_menu(user)
+
+      if int(choice) ==1:
+       add_member(name,rank,div,id)
+      elif int(choice) ==2:
+        remove_member(name,rank,div,id)
+      elif int(choice) ==3:
+        update_rank(name,rank,div,id)
+      elif int(chooice) ==4:
+         display_roster(name,rank,div,id)
+      elif int(choice) ==5:
+        search_crew(name,rank,div,id)
+      elif int(choice) ==6:
+        filter_by_division(name,rank,div,id)
+      elif int(choice) ==7:
+        print(f"total payroll: {calculate_payroll(ranks)} british_pounds\n")
+      elif int(choice) ==8:
+       print(f"Senior officers: {count_officers(ranks)}\n")
+      elif int(choice) ==9:
+        print("excape") 
+        break
+      
+      
 
 
 
